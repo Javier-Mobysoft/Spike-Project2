3b72aaf14fa5990c8996b13fd92bd463693047c1
Javier <javier.gomez@mobysoft.com>
2017-01-11 15:02:04 +0000

    Initial Version

---
+++ src/main/java/com/gitflow/spike/project2/Service2.java
@@ -0,0 +1,14 @@
+package com.gitflow.spike.project2;
+
+
+import com.gitflow.spike.common.Util;
+
+/**
+ * Created by Javier on 11/01/2017.
+ */
+public class Service2 {
+
+    public void  execute(){
+        Util.print();
+    }
+}
